f2a8eab145d81460e097d0685640898d4f562271
Alex Havermale <alex.havermale@icloud.com>
2017-07-20 16:02:28 +0900

    Initial commit.

---
+++ SpotifyKit/SpotifyKit.h
@@ -0,0 +1,19 @@
+//
+//  SpotifyKit.h
+//  SpotifyKit
+//
+//  Created by Alexander Havermale on 7/20/17.
+//  Copyright © 2017 Alex Havermale. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SpotifyKit.
+FOUNDATION_EXPORT double SpotifyKitVersionNumber;
+
+//! Project version string for SpotifyKit.
+FOUNDATION_EXPORT const unsigned char SpotifyKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SpotifyKit/PublicHeader.h>
+
+
