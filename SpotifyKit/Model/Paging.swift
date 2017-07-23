fd4f9ef8c3869a6a6a5bcc7567e5d4d7c0c6a479
Alex Havermale <alex.havermale@icloud.com>
2017-07-23 14:11:35 +0900

    Extends Paging object to include Collection logic.

--- SpotifyKit/Model/Paging.swift
+++ SpotifyKit/Model/Paging.swift
@@ -44,3 +44,99 @@ public struct Paging<Object/*: Codable*/> { // TODO: Make JSON Codable.
     }
 }
 
+// MARK: - Paging + Collection Conformance
+
+extension Paging: Collection { // Forwards Collection logic to the Paging structure for convenience.
+    
+    // TODO: Test how default implementations would work without explicitly defining functions.
+
+    public typealias Items = Array<Object>
+    public typealias IndexDistance = Items.IndexDistance
+    public typealias Indices = Items.Indices
+    public typealias Iterator = Items.Iterator
+    public typealias SubSequence = Items.SubSequence
+
+    // MARK: - Instance Properties
+
+    public var startIndex: Int { return items.startIndex }      // Required by _IndexableProtocol.
+    public var endIndex: Int { return items.endIndex }          // Required by _IndexableProtocol.
+    public var first: Items.Element? { return items.first }     // Required by Collection.
+    public var indices: Items.Indices { return items.indices }  // Required by Collection.
+    public var isEmpty: Bool { return items.isEmpty }           // Required by Collection.
+    public var lazy: LazyCollection<Items> { return items.lazy }
+
+    // MARK: - Instance Methods
+
+    // Required by Collection.
+    public func distance(from start: Items.Index, to end: Items.Index) -> Items.IndexDistance {
+        return items.distance(from: start, to: end)
+    }
+    // Required by Collection.
+    public func index(_ i: Items.Index, offsetBy n: Items.IndexDistance) -> Items.Index {
+        return items.index(i, offsetBy: n)
+    }
+    // Required by Collection.
+    public func index(_ i: Items.Index, offsetBy n: Items.IndexDistance, limitedBy limit: Items.Index) -> Items.Index? {
+        return items.index(i, offsetBy: n, limitedBy: limit)
+    }
+    // Required by _IndexableProtocol.
+    public func index(after i: Int) -> Int {
+        return items.index(after: i)
+    }
+    // Does not apply.
+    //public func index(of element: Items.Element) -> Items.Index? {
+    //    return items.index(of: element)
+    //}
+    // Not required.
+    public func index(where predicate: (Items.Element) throws -> Bool) rethrows -> Items.Index? {
+        return try items.index(where: predicate)
+    }
+    // Does not apply. (Objects won't be collections themselves.)
+    //public func joined() -> FlattenCollection<Items> {
+    //    return items.joined()
+    //}
+    // Not required.
+    public func makeIterator() -> Items.Iterator {
+        return items.makeIterator()
+    }
+    // Required by Collection.
+    public func prefix(through position: Items.Index) -> Items.SubSequence {
+        return items.prefix(through: position)
+    }
+    // Required by Collection.
+    public func prefix(upTo end: Items.Index) -> Items.SubSequence {
+        return items.prefix(upTo: end)
+    }
+    // Required by Collection.
+    public func suffix(from start: Items.Index) -> Items.SubSequence {
+        return items.suffix(from: start)
+    }
+    
+    // MARK: Subscripts
+
+    // Required by Collection.
+    public subscript(position: Items.Index) -> Items.Element {
+        return items[position]
+    }
+    // Required by Collection.
+    public subscript(bounds: Range<Items.Index>) -> Items.SubSequence {
+        return items[bounds]
+    }
+}
+
+extension Paging: RandomAccessCollection {
+    // TODO: Test how default implementations would work without explicitly defining functions.
+}
+
+extension Paging: BidirectionalCollection {
+    // TODO: Test how default implementations would work without explicitly defining functions.
+}
+
+// MARK: - Paging + Sequence Conformance
+
+extension Paging: Sequence { // Forwards Sequence logic (that wasn't already defined in 'Collection') to the Paging structure for convenience.
+    
+    public typealias Element = Items.Element
+    
+    // TODO: Test how default implementations would work without explicitly defining functions.
+}
