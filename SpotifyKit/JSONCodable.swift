d2153b15727d8399b95364ae52df1b3ca88cacb4
Alex Havermale <alex.havermale@icloud.com>
2017-07-31 14:31:44 +0900

    Removes KeyedDecodingContainer extension.

--- SpotifyKit/JSONCodable.swift
+++ SpotifyKit/JSONCodable.swift
@@ -34,86 +34,17 @@ public protocol JSONEncodable: Encodable {
     // TODO: Design JSONEncodable.
 }
 
-// MARK: - Keyed Decoding Container + Raw Value Case Conversion
+// MARK: - Raw Representable Decoding + Case Tolerance
 //
 // In some instances, certain objects returned by the Web API contain strings
 // whose formatting differs from their expected or documented values. For
 // example, when requesting "recommendations based on seeds," cartain fields
 // that are otherwise always lowercased strings are returned in all caps —
 // making raw representable types based on these fields hard to instantiate.
-// These methods, when used in a custom decoding initializer, provide better,
-// more precise control over how these enum-dependent raw values are decoded.
-//extension KeyedDecodingContainer {
-//
-//    enum StringDecodingStrategy {
-//        case capitalize
-//        case lowercase
-//        case uppercase
-//    }
-//
-//    /// Decodes a value of the given string representable type for the given key, changing case if necessary.
-//    ///
-//    /// - parameter type: The type of value to decode.
-//    /// - parameter key: The key that the decoded value is associated with.
-//    /// - parameter case: The case the raw string value must be converted to in order to initialize an instance of the given type.
-//    /// - returns: A value of the requested type, if present for the given key and convertible to the requested case and type.
-//    /// - throws: `DecodingError.dataCorrupted` if the encountered value is an invalid raw value for the given type.
-//    func decode<T: Decodable>(_ type: T.Type, forKey key: K, toCase case: StringDecodingStrategy? = nil) throws -> T where T: RawRepresentable, T.RawValue == String {
-//
-//        let decoded = try self.decode(String.self, forKey: key)
-//
-//        let rawValue: String
-//        switch `case` {
-//            case .some(.capitalize): rawValue = decoded.capitalized
-//            case .some(.lowercase): rawValue = decoded.lowercased()
-//            case .some(.uppercase): rawValue = decoded.uppercased()
-//            case .none: rawValue = decoded
-//        }
-//
-//        guard let value = type.init(rawValue: rawValue) else {
-//            throw DecodingError.dataCorrupted(DecodingError.Context(codingPath: self.codingPath + [key], debugDescription: "Cannot initialize \(T.self) from invalid \(T.RawValue.self) value \"\(decoded)\" or any case-sensitive variant"))
-//        }
-//
-//        return value
-//    }
-//
-//    /// Decodes a value of the given string representable type for the given key and changing case if necessary, if present.
-//    ///
-//    /// This method returns `nil` if the container does not have a value associated with `key`, or if the value is null. The difference between these states can be distinguished with a `contains(_:)` call.
-//    ///
-//    /// - parameter type: The type of value to decode.
-//    /// - parameter key: The key that the decoded value is associated with.
-//    /// - parameter case: The case the raw string value must be converted to in order to initialize an instance of the given type.
-//    /// - returns: A decoded value of the requested type, or `nil` if the `Decoder` does not have an entry associated with the given key, or if the value is a null value.
-//    /// - throws: `DecodingError.dataCorrupted` if the encountered value is an invalid raw value for the given type.
-//    func decodeIfPresent<T: Decodable>(_ type: T.Type, forKey key: K, toCase case: StringDecodingStrategy? = nil) throws -> T? where T: RawRepresentable, T.RawValue == String {
-//
-//        guard let decoded = try self.decodeIfPresent(String.self, forKey: key) else { return nil }
-//
-//        let rawValue: String
-//        switch `case` {
-//            case .some(.capitalize): rawValue = decoded.capitalized
-//            case .some(.lowercase): rawValue = decoded.lowercased()
-//            case .some(.uppercase): rawValue = decoded.uppercased()
-//            case .none: rawValue = decoded
-//        }
-//
-//        guard let value = type.init(rawValue: rawValue) else {
-//            throw DecodingError.dataCorrupted(DecodingError.Context(codingPath: self.codingPath + [key], debugDescription: "Cannot initialize \(T.self) from invalid \(T.RawValue.self) value \"\(decoded)\" or any case-sensitive variant"))
-//        }
-//
-//        return value
-//    }
-//}
-
-// MARK: - Raw Representable Decoding + Case Tolerance
-//
-// In lieu of using the KeyedDecodingContainer extension above, the following
-// approach provides a more succinct solution to the case-sensitive conflicts
-// between raw values and raw representable types. Instead of specifying a case
-// when decoding each property individually, this approach implicitly converts
-// between lowercase, uppercase, and sentence cases before throwing an error
-// any time we decode a raw representable type.
+// The following extension provides a succinct solution to this case-sensitive
+// conflict by implicitly converting each raw value between lowercase,
+// uppercase, and sentence cases before throwing an error any time we decode
+// a raw representable type.
 //
 extension RawRepresentable where RawValue == String, Self: Decodable {
     public init(from decoder: Decoder) throws {
