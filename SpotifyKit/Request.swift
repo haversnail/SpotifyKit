31f3a2dd31b50cc75c825e1c1a973902ff28888f
Alex Havermale <alex.havermale@icloud.com>
2017-09-12 15:57:26 +0900

    Moves the 'Constants' struct out of the Request class and into the global scope, adding all API endpoints as well.
    • Creates a fileprivate method overriding the infix “/“ operator for
    string types, providing better clarity and easy-to-read string
    concatenation for the endpoint paths.

--- SpotifyKit/Request.swift
+++ SpotifyKit/Request.swift
@@ -157,32 +157,6 @@ public typealias SKDecodedRequestHandler<T: JSONDecodable> = (_ object: T?, _ er
 /// [SLRequest]: apple-reference-documentation://hsp4_7YsJD
 public class SKRequest { // Inheriting from NSObject causes buildtime error: class conflicts with StoreKit's `SKRequest` class.
     
-    private struct Constants {
-        private init() {}
-        static let baseURL = URL(string: "https://api.spotify.com")!
-        static let accountsURL = URL(string: "https://accounts.spotify.com")!
-        static let tokenType = "Bearer"
-        struct HeaderFields {
-            private init() {}
-            static let authorization = "Authorization"
-            static let contentType = "Content-Type"
-            //static let matchETag = "If-None-Match"
-        }
-        struct QueryParameters {
-            private init() {}
-            static let query = "q"
-            static let market = "market"
-            static let limit = "limit"
-            static let offset = "offset"
-            static let itemType = "type"
-        }
-        //struct ContentTypes {
-        //    private init() {}
-        //    static let json = "application/json"
-        //    static let jpeg = "image/jpeg"
-        //}
-    }
-    
     /// The MIME content types available for sending multipart requests to the [Spotify Web API][API].
     ///
     /// - Note: The only content types currently accepted by the [Spotify Web API][API] are "`application/json`" and "`image/jpeg`" for all PUT, POST, or DELETE requests. If/when more content types become part of the API, they will be made available here.
@@ -714,6 +688,104 @@ extension Expandable where Self: JSONDecodable {
     public func getFullObject(handler: @escaping (Self?, Error?) -> Void) {
         guard isSimplified else { return }
         makeFullObjectRequest().perform(handler: handler)
+    }
+}
+
+// MARK: - Web API Constants
+
+internal struct Constants {
+    private init() {}
+    static let baseURL = URL(string: "https://api.spotify.com")!
+    static let accountsURL = URL(string: "https://accounts.spotify.com")!
+    static let tokenType = "Bearer"
+    struct HeaderFields {
+        private init() {}
+        static let authorization = "Authorization"
+        static let contentType = "Content-Type"
+        //static let matchETag = "If-None-Match"
+    }
+    //struct ContentTypes {
+    //    private init() {}
+    //    static let json = "application/json"
+    //    static let jpeg = "image/jpeg"
+    //}
+    struct QueryParameters {
+        private init() {}
+        static let ids = "ids"
+        static let query = "q"
+        static let country = "country"
+        static let market = "market"
+        static let limit = "limit"
+        static let offset = "offset"
+        static let itemType = "type"
+        static let albumType = "album_type"
+    }
+    struct Endpoints {
+        private init() {}
+        private static let version = "v1"
         
+        static var albums: String             { return version/"albums" }
+        static var artists: String            { return version/"artists" }
+        static var audioAnalysis: String      { return version/"audio-analysis" }
+        static var audioFeatures: String      { return version/"audio-features" }
+        static var featuredPlaylists: String  { return version/"browse/featured-playlists" }
+        static var newReleases: String        { return version/"browse/new-releases" }
+        static var categories: String         { return version/"browse/categories" }
+        static var me: String                 { return version/"me" }
+        static var recommendations: String    { return version/"recommendations" }
+        static var search: String             { return version/"search" }
+        static var tracks: String             { return version/"tracks" }
+        static var users: String              { return version/"users" }
+        
+        static var myFollows: String          { return me/"following" }
+        static var mySavedAlbums: String      { return me/"albums" }
+        static var mySavedTracks: String      { return me/"tracks" }
+        static var myTopArtists: String       { return me/"top/artists" }
+        static var myTopTracks: String        { return me/"top/tracks" }
+        static var myPlaylists: String        { return me/"playlists" }
+        static var player: String             { return me/"player" }
+        
+        static var checkIfFollowing: String   { return myFollows/"contains" }
+        static var checkIfAlbumsSaved: String { return mySavedAlbums/"contains" }
+        static var checkIfTracksSaved: String { return mySavedTracks/"contains" }
+        
+        static var recentlyPlayed: String     { return player/"recently-played" }
+        static var currentlyPlaying: String   { return player/"currently-playing" }
+        static var availableDevices: String   { return player/"devices" }
+        static var play: String               { return player/"play" }
+        static var pause: String              { return player/"pause" }
+        static var next: String               { return player/"next" }
+        static var previous: String           { return player/"previous" }
+        static var seek: String               { return player/"seek" }
+        static var volume: String             { return player/"volume" }
+        static var shuffle: String            { return player/"shuffle" }
+        static var `repeat`: String           { return player/"repeat" }
+        
+        static func album(id: String) -> String                   { return albums/id }
+        static func tracksForAlbum(id: String) -> String          { return albums/id/"tracks" }
+        static func artist(id: String) -> String                  { return artists/id }
+        static func albumsForArtist(id: String) -> String         { return artists/id/"albums" }
+        static func topTracksForArtist(id: String) -> String      { return artists/id/"top-tracks" }
+        static func relatedArtistsForArtist(id: String) -> String { return artists/id/"related-artists" }
+        static func audioAnalysisForTrack(id: String) -> String   { return audioAnalysis/id }
+        static func audioFeaturesForTrack(id: String) -> String   { return audioFeatures/id }
+        static func category(id: String) -> String                { return categories/id }
+        static func playlistsForCategory(id: String) -> String    { return categories/id/"playlists" }
+        static func track(id: String) -> String                   { return tracks/id }
+        static func user(id: String) -> String                    { return users/id }
+        
+        private static let playlists = "playlists"
+        static func playlistsForUser(id: String) -> String                            { return users/id/playlists }
+        static func playlist(id: String, ownerID: String) -> String                   { return users/ownerID/playlists/id }
+        static func imageForPlaylist(id: String, ownerID: String) -> String           { return users/ownerID/playlists/id/"images" }
+        static func tracksForPlaylist(id: String, ownerID: String) -> String          { return users/ownerID/playlists/id/"tracks" }
+        static func followsForPlaylist(id: String, ownerID: String) -> String         { return users/ownerID/playlists/id/"followers" }
+        static func checkIfUsersFollowPlaylist(id: String, ownerID: String) -> String { return users/ownerID/playlists/id/"followers/contains" }
+    }
+}
+
+extension String {
+    fileprivate static func /(lhs: String, rhs: String) -> String {
+        return lhs + "/" + rhs
     }
 }
