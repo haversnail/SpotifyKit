f17ee0706bbd21ac60f4ce339716b3ac093bad5e
Alex Havermale <alex.havermale@icloud.com>
2017-11-05 21:43:58 +0900

    Reorganizes project for beta model and requests.

--- SpotifyKit/Model/Beta/PlaybackEvent.swift
+++ SpotifyKit/Model/Beta/PlaybackEvent.swift
@@ -80,3 +80,117 @@ extension SKPlaybackEvent: Decodable {
         playbackDate = date
     }
 }
+
+
+
+
+// MARK: - "Currently Playing Context" (βeta)
+// FIXME: Rename and consider waiting to implement.
+
+public struct SKPlaybackState: JSONDecodable { // SPTPlaybackState
+    
+    /// The device that is currently active.
+    public let device: SKDevice // Optional in some responses
+    
+    ///
+    public let repeatMode: SPTRepeatMode // Optional in some responses
+    
+    ///
+    public let isShuffling: Bool // Optional in some responses
+    
+    /// ...can be `nil`.
+    public let context: SKPlaybackEvent.PlaybackContext?
+    
+    /// The timestamp when the data was fetched.
+    public let timestamp: Date // provided as int??
+    
+    
+    private let _progress: Int?
+    
+    /// The progress into the currently playing track. If no track is currently playing, this will be `nil`.
+    public var progress: TimeInterval? {
+        switch _progress {
+            case .some(let t): return TimeInterval(t) / 1000
+            case .none: return nil
+        }
+    }
+    
+    /// A Boolean value indicating whether a track is currently playing.
+    public let isPlaying: Bool
+    
+    /// The currently playing track. If no track is currently playing, this will be `nil`.
+    public let track: SKTrack?
+    
+    // MARK: - Keys
+    
+    private enum CodingKeys: String, CodingKey {
+        case device
+        case repeatMode = "repeat_state"
+        case isShuffling = "shuffle_state"
+        case context
+        case timestamp
+        case _progress = "progress_ms"
+        case isPlaying = "is_playing"
+        case track = "item"
+    }
+}
+
+// MARK: - SPTRepeatMode Coding & Mapping
+
+extension SPTRepeatMode: Codable {
+    public init?(rawValue: String) {
+        switch rawValue {
+            case "off": self = .off
+            case "context": self = .context
+            case "track": self = .one
+            default: return nil
+        }
+    }
+}
+
+public protocol SPTConvertible {
+    associatedtype ReferenceType
+    var sptInstance: ReferenceType { get }
+}
+
+extension SKPlaybackState: SPTConvertible {
+    public typealias ReferenceType = SPTPlaybackState
+    
+    public var sptInstance: SPTPlaybackState {
+        return SPTPlaybackState(isPlaying: isPlaying,
+                                isRepeating: repeatMode == .context || repeatMode == .one,
+                                isShuffling: isShuffling,
+                                isActiveDevice: device.type == .mobile, // No good... find a better way.
+                                position: TimeInterval(_progress ?? 0))!
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
