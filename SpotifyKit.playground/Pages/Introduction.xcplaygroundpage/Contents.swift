2b6858d1fe75f22a4e6b91bfb303e09777bc2926
Alex Havermale <alex.havermale@icloud.com>
2017-11-13 19:47:13 +0900

    Sets up playground pages.

---
+++ SpotifyKit.playground/Pages/Introduction.xcplaygroundpage/Contents.swift
@@ -0,0 +1,24 @@
+/*:
+ ![Logo](Logo.png)
+ # SpotifyKit Playgrounds
+ *****
+ 
+ ## Table of Contents
+ - [Making API Requests](Making%20API%20Requests)
+ - [Decoding Responses](Decoding%20Responses)
+ - [Pagination](Pagination)
+ - [`SKCatalog` & Other Request Factories](Catalog%20&%20Other%20Request%20Factories)
+ - [SDK Integration](SDK%20Integration)
+ - [LICENSE](LICENSE)
+ 
+ > ### Before using this Playground:
+ 1. ...
+ 2. ...
+ 3. ...
+ 
+ */
+
+import SpotifyKit
+import Foundation
+
+//: [Next](@next)
---
+++ SpotifyKit.playground/Pages/Making API Requests.xcplaygroundpage/Contents.swift
@@ -0,0 +1,8 @@
+//: [Table of Contents](Introduction) | [Previous](@previous) | [Next](@next)
+//: *****
+
+import SpotifyKit
+import Foundation
+
+//: *****
+//: [Table of Contents](Introduction) | [Previous](@previous) | [Next](@next)
