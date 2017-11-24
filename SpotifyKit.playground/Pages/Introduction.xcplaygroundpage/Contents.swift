adc2b5c27848f87e09ed7160ef36649e8131fd5e
Alex Havermale <alex.havermale@icloud.com>
2017-11-24 15:21:40 +0900

    Finishes the SDK Integration Playground page.

--- SpotifyKit.playground/Pages/Introduction.xcplaygroundpage/Contents.swift
+++ SpotifyKit.playground/Pages/Introduction.xcplaygroundpage/Contents.swift
@@ -8,7 +8,7 @@
  - [Decoding Responses](Decoding%20Responses)
  - [`SKCatalog` & Other Request Factories](Request%20Factories)
  - [Pagination](Pagination)
- - [SDK Integration](SDK%20Integration)
+ - [Spotify iOS SDK Integration](SDK%20Integration)
  - [LICENSE](LICENSE)
  
  - Important:
@@ -16,7 +16,7 @@
     1. Using [Carthage](https://github.com/Carthage/Carthage), check out the [Spotify iOS SDK](https://github.com/spotify/ios-sdk) dependency (if you haven't already) by running `carthage bootstrap --no-build` from the repo's root directory.
     2. Open `SpotifyKit.xcworkspace`.
     3. Build the current scheme (**"Product" > "Build"** or **⌘B**).
-    4. Grab a valid Web API Access Token [here](https://developer.spotify.com/web-api/console/get-current-user/token?scope=user-read-private&scope=user-read-birthdate&scope=user-read-email&scope=playlist-read-private&scope=playlist-read-collaborative&scope=playlist-modify-public&scope=playlist-modify-private&scope=user-library-read&scope=user-library-modify&scope=user-follow-read&scope=user-follow-modify&scope=user-top-read&scope=user-read-playback-state&scope=user-read-recently-played&scope=user-read-currently-playing&scope=user-modify-playback-state&scope=ugc-image-upload). (P.S. - it's only good for an hour.)
+    4. Grab a valid Web API Access Token [here](https://developer.spotify.com/web-api/console/get-current-user/token?scope=user-read-private&scope=user-read-birthdate&scope=user-read-email&scope=playlist-read-private&scope=playlist-read-collaborative&scope=playlist-modify-public&scope=playlist-modify-private&scope=user-library-read&scope=user-library-modify&scope=user-follow-read&scope=user-follow-modify&scope=user-top-read&scope=user-read-playback-state&scope=user-read-recently-played&scope=user-read-currently-playing&scope=user-modify-playback-state&scope=ugc-image-upload&scope=streaming). (P.S. - it's only good for an hour.)
     5. Open `Setup.swift` in the playground's **Sources** folder.
     6. Paste your Spotify Premium account username and shiny new access token in the placeholders.
     7. You're good to go! 👍
--- SpotifyKit.playground/Pages/SDK Integration.xcplaygroundpage/Contents.swift
+++ SpotifyKit.playground/Pages/SDK Integration.xcplaygroundpage/Contents.swift
@@ -3,21 +3,84 @@
  ***
  
  # Spotify iOS SDK Integration
- Introduction goes here.
+ > ### Running this playground will begin audio playback—make sure your speakers are ready! 🔇🔊
+ */
+/// A Spotify client ID to use for the Playground's audio streaming controller.
+let clientID = "<#Your App's Client ID#>"
+/*:
+ While its primary purpose is providing a Swift wrapper for the [Web API](https://developer.spotify.com/web-api/), **SpotifyKit** also aims to assist in integrating the [Spotify iOS SDK](https://github.com/spotify/ios-sdk) into your project as well. With a little magic and some Swift extensions, it streamlines the inclusion of Spotify's authentication and audio playback frameworks, and adds convenience methods to existing iOS SDK types for compatibility and interoperatbility.
  
- ## Authentication
- ...
- 
- ## Audio Playback
- ...
- 
- ## Roadmap
- ...
- 
+ ## Module Availability
+ As you've seen throughout these playground pages, importing `SpotifyKit` in your code allows access to all the symbols declared in the `SpotifyAuthentication` and `SpotifyAudioPlayback` frameworks:
  */
 
 import SpotifyKit
-import Foundation
 
+SPTAuth.defaultInstance().session = SPTSession(userName: username, accessToken: accessToken, expirationDate: Date.distantFuture)
+
+//: - Important: In order for this to work correctly, it's important that the `SpotifyKit` module be built using the current scheme. Check out the README's [note on module maps](../README.md#a-note-on-module-maps) for more details.
+
+/*:
+ ## Authentication
+ As mentioned in [Making API Requests](Making%20API%20Requests), the `SKRequest` class uses an [`SPTSession`](https://spotify.github.io/ios-sdk/Classes/SPTSession.html) instance to authenticate its underlying URL request. Although custom session objects would normally be assigned after the creation of a request, **SpotifyKit** also adds the flexibility to create a request directly from the session object itself:
+ */
+
+import Foundation
+import PlaygroundSupport
+
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+let session = SPTAuth.defaultInstance().session!
+let request = session.makeRequest(method: .GET, endpoint: "/v1/artists/6YWdHD3R863Apw1hkx3BwC")
+
+//: Or, you could use the `performRequest` method, handling preparation and execution all at once:
+
+session.performRequest(method: .GET, endpoint: "/v1/artists/4FZ3j1oH43e7cukCALsCwf") { (data, status, error) in
+    // Handle thangs.
+}
+
+/*:
+ ## Audio Playback
+ At the end of the day, what good would all this be if you can't play anything? 🤔
+ 
+ **SpotifyKit** also attempts to provide interoperability with the [`SPTAudioStreamingController`](https://spotify.github.io/ios-sdk/Classes/SPTAudioStreamingController.html) class as well, adding to it a set of playback and queueing methods that take **SpotifyKit** types as arguments. As you'll see (and hear) below, we now have a `play` method on our player instance which, in this example, takes any type that conforms to the `Album` protocol, and optionally, the index of the track with which we want to begin playback:
+ */
+
+let player = SPTAudioStreamingController.sharedInstance()!
+try? player.start(withClientId: clientID)
+player.login(withAccessToken: accessToken)
+
+sleep(5) // Gotta wait a second (or five) for the player to login...
+
+SKCatalog.local.getAlbum(withID: "5DLhV9yOvZ7IxVmljMXtNm") { [unowned player] (album, error) in
+    
+    if let album = album {
+        // Plays the album, starting at the third track (or rather, the track at index "2"):
+        player.play(album, at: 2) { (error) in
+            // Any errors encountered attempting playback are provided here.
+            if let error = error {
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
+
+sleep(10)
+
+//: Alternatively, **SpotifyKit** also makes this functionality available as a method on the values themselves:
+
+SKCatalog.local.getTrack(withID: "1aJtyIMev4kW8IMUxOjn1Y") { (track, error) in
+    
+    track?.queue { (error) in
+        // Got errors? Handle 'em here!
+    }
+}
+
+/*:
+ ## The Road Ahead
+ As Spotify continues to expand upon their developer resources and platform SDKs, **SpotifyKit** will also aim to stay relevant with what's available. In the meantime, you should check out their [beta website](https://beta.developer.spotify.com/) for developers—if **SpotifyKit** doesn't suit your app's specific needs, perhaps there you'll find something that can!
+ 
+ You can check out the detailed roadmap for **SpotifyKit** in the [README](README.md).
+ */
 //: ***
 //: [Table of Contents](Introduction) | [Previous](@previous) | [Next](@next)
